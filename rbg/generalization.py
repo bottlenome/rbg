b23cf5af21fcd99d5ec0c221b75e852eec910a80
bottlenome <bottlenome@gmail.com>
2021-09-02 11:02:23 +0900

    Fix backward error

--- rbg/generalization.py
+++ rbg/generalization.py
@@ -4,17 +4,52 @@ import torch.nn.functional as F
 from .function import onehot, onehot_cross
 
 
-class random_batch_generalization(torch.autograd.Function):
+class random_batch_generalization_abs(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x, y, rate, epsilon, abs_mag=False):
+    def forward(ctx, x, y, rate, epsilon):
         batch_size = x.shape[0]
         ref_index = torch.randint(low=0, high=batch_size - 1,
                                   size=(int(batch_size * rate), ))
         target_index = torch.randint(low=0, high=batch_size - 1,
                                      size=(int(batch_size * rate), ))
         mag = torch.empty(len(ref_index)).normal_(mean=0.0, std=epsilon)
-        if abs_mag:
-            mag = mag.abs()
+        mag = mag.abs()
+        ctx.save_for_backward(x, ref_index, target_index, mag)
+        ret = x.clone()
+        ret_y = y.clone()
+        for i in range(len(ref_index)):
+            ret[ref_index[i]] = (x[target_index[i]] * mag[i]
+                                 + x[ref_index[i]] * (1 - mag[i]))
+
+            total = (mag[i].abs() + (1 - mag[i]).abs())
+            target_p = mag[i].abs() / total
+            ref_p = (1 - mag[i]).abs() / total
+            ret_y[target_index[i]] += y[ref_index[i]] * target_p
+            ret_y[ref_index[i]] = y[ref_index[i]] * ref_p
+        return ret, ret_y
+
+    @staticmethod
+    def backward(ctx, grad_output, _):
+        x, ref_index, target_index, mag = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        for i in range(len(ref_index)):
+            ref = grad_input[ref_index[i]]
+            # dL/da = dL/dy * dy/da
+            grad_input[ref_index[i]] = ref * (1 - mag[i])
+            # dL/db = dL/dy * dy/db
+            grad_input[target_index[i]] += ref * mag[i]
+        return grad_input, None, None, None
+
+
+class random_batch_generalization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, y, rate, epsilon):
+        batch_size = x.shape[0]
+        ref_index = torch.randint(low=0, high=batch_size - 1,
+                                  size=(int(batch_size * rate), ))
+        target_index = torch.randint(low=0, high=batch_size - 1,
+                                     size=(int(batch_size * rate), ))
+        mag = torch.empty(len(ref_index)).normal_(mean=0.0, std=epsilon)
         ctx.save_for_backward(x, ref_index, target_index, mag)
         ret = x.clone()
         ret_y = y.clone()
@@ -47,8 +82,10 @@ class RandomBatchGeneralization(nn.Module):
         super().__init__()
         self.epsilon = epsilon
         self.rate = rate
-        self.forward_ = random_batch_generalization.apply
-        self.abs_mag = abs_mag
+        if abs_mag:
+            self.forward_ = random_batch_generalization.apply
+        else:
+            self.forward_ = random_batch_generalization_abs.apply
 
     def forward(self, x, y):
         if self.training:
